bfba3b96e2d3ed87d987e973f3c3b8a1053fd078
Mohit Kumar Yadav <dearmky@gmail.com>
2025-07-04 17:45:59 +0530

    Update config.php

--- src/config.php
+++ src/config.php
@@ -1,1 +1,11 @@
-
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && current_user_can('manage_options')) {
+    $creds = [
+        'apiKey' => sanitize_text_field($_POST['apiKey']),
+        'authDomain' => sanitize_text_field($_POST['authDomain']),
+        'projectId' => sanitize_text_field($_POST['projectId']),
+        'messagingSenderId' => sanitize_text_field($_POST['messagingSenderId']),
+        'appId' => sanitize_text_field($_POST['appId'])
+    ];
+    update_option('wpfp_firebase_credentials', $creds);
+    echo "<div class='updated'><p>Saved!</p></div>";
+}
