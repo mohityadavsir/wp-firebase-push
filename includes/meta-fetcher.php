0716ff723c802746bf1dae1a4a46da23f93544f6
Mohit Kumar Yadav <dearmky@gmail.com>
2025-07-04 17:44:46 +0530

    Update meta-fetcher.php

--- includes/meta-fetcher.php
+++ includes/meta-fetcher.php
@@ -1,1 +1,15 @@
+add_action('wp_ajax_wpfp_fetch_meta', function () {
+    $url = esc_url_raw($_GET['url']);
+    $html = file_get_contents($url);
 
+    preg_match('/<meta property="og:title" content="(.*?)"/', $html, $title);
+    preg_match('/<meta property="og:description" content="(.*?)"/', $html, $desc);
+    preg_match('/<meta property="og:image" content="(.*?)"/', $html, $img);
+
+    echo json_encode([
+        'title' => $title[1] ?? '',
+        'description' => $desc[1] ?? '',
+        'image' => $img[1] ?? ''
+    ]);
+    wp_die();
+});
